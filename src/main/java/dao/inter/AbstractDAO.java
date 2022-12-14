b9403ecd8e4b4b6f2231b8f2602d742f2f3cc72f
Subhan <79940729+SubhanRzayev@users.noreply.github.com>
2022-12-14 18:36:02 +0400

    Update AbstractDAO.java

--- src/main/java/dao/inter/AbstractDAO.java
+++ src/main/java/dao/inter/AbstractDAO.java
@@ -7,7 +7,7 @@ public abstract class AbstractDAO {
     public static Connection connect() throws Exception {
         String url = "jdbc:mysql://localhost:3306/resume";
         String username = "root";
-        String password = "P@ssw0rd";
+        String password = "";
         Connection c = DriverManager.getConnection(url,username,password);
         return c;
     }
